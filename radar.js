5803c1981935c35717a7616d5ee2e5134d06d58d
tw8821516-hub <tw8821516@gmail.com>
2026-03-08 19:32:35 +0800

    Update radar.js

--- radar.js
+++ radar.js
@@ -1,15 +1,9 @@
-const symbols = [
-"SPY","QQQ","EEM","TLT","HYG",
-"GLD","SLV","USO","UUP","FXE"
-];
+const TOKEN = "7341786399";
+const CHAT_ID = "8522391684:AAFhKj3jLdt9Gd3qL8yAwv8PLK-OclVnjZM";
 
-const TOKEN = "你的BOT_TOKEN";
-const CHAT_ID = "你的CHAT_ID";
+async function sendTelegram(msg){
 
-async function sendTelegram(message){
-
-const url =
-`https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
 await fetch(url,{
 method:"POST",
@@ -18,72 +12,9 @@ headers:{
 },
 body:JSON.stringify({
 chat_id:CHAT_ID,
-text:message
+text:msg
 })
 });
-
-}
-
-async function getData(symbol){
-
-const url =
-`https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?interval=1h&range=60d`;
-
-const res = await fetch(url);
-const data = await res.json();
-
-let prices =
-data.chart.result[0].indicators.quote[0].close;
-
-prices = prices.filter(p => p);
-
-function MA(data,period){
-
-let result=[];
-
-for(let i=0;i<data.length;i++){
-
-if(i<period-1){
-result.push(null);
-}else{
-
-let slice=data.slice(i-period+1,i+1);
-let avg=slice.reduce((a,b)=>a+b)/period;
-
-result.push(avg);
-
-}
-
-}
-
-return result;
-
-}
-
-const ma240 = MA(prices,240);
-
-const last = ma240.at(-1);
-const prev = ma240.at(-2);
-
-let signal="NONE";
-
-if(prev && last){
-
-if(prev<=last && last>prev){
-signal="MA240_UP";
-}
-
-if(prev>=last && last<prev){
-signal="MA240_DOWN";
-}
-
-}
-
-return {
-symbol,
-ma240:last,
-signal
-};
 
 }
 
@@ -93,7 +24,7 @@ let results=[];
 
 for(let s of symbols){
 
-let r=await getData(s);
+let r = await getData(s);
 
 if(r.signal!=="NONE"){
 results.push(r);
