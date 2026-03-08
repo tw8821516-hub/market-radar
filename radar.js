a31c65866c7f8a57c97179986e87d0686546be22
tw8821516-hub <tw8821516@gmail.com>
2026-03-08 19:37:04 +0800

    Update radar.js

--- radar.js
+++ radar.js
@@ -1,5 +1,10 @@
-const TOKEN = "7341786399";
-const CHAT_ID = "8522391684:AAFhKj3jLdt9Gd3qL8yAwv8PLK-OclVnjZM";
+const symbols = [
+"SPY","QQQ","EEM","TLT","HYG",
+"GLD","SLV","USO","UUP","FXE"
+];
+
+const TOKEN = "8522391684:AAFhKj3jLdt9Gd3qL8yAwv8PLK-OclVnjZM";
+const CHAT_ID = "7341786399";
 
 async function sendTelegram(msg){
 
@@ -15,6 +20,69 @@ chat_id:CHAT_ID,
 text:msg
 })
 });
+
+}
+
+async function getData(symbol){
+
+const url =
+`https://query1.finance.yahoo.com/v8/finance/chart/${symbol}?interval=1h&range=60d`;
+
+const res = await fetch(url);
+const data = await res.json();
+
+let prices =
+data.chart.result[0].indicators.quote[0].close;
+
+prices = prices.filter(p => p);
+
+function MA(data,period){
+
+let result=[];
+
+for(let i=0;i<data.length;i++){
+
+if(i<period-1){
+result.push(null);
+}else{
+
+let slice=data.slice(i-period+1,i+1);
+let avg=slice.reduce((a,b)=>a+b)/period;
+
+result.push(avg);
+
+}
+
+}
+
+return result;
+
+}
+
+const ma240 = MA(prices,240);
+
+const last = ma240.at(-1);
+const prev = ma240.at(-2);
+
+let signal="NONE";
+
+if(prev && last){
+
+if(prev<=last && last>prev){
+signal="MA240_UP";
+}
+
+if(prev>=last && last<prev){
+signal="MA240_DOWN";
+}
+
+}
+
+return {
+symbol,
+ma240:last,
+signal
+};
 
 }
 
@@ -49,6 +117,10 @@ message+=`${r.symbol} ▼ MA240 DOWN\n`;
 }
 
 await sendTelegram(message);
+
+}else{
+
+await sendTelegram("📡 Market Radar\nNo signal");
 
 }
 
